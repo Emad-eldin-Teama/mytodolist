4c5ea1f81c4f403485d2d2950a29e002205afde3
emad el din Emadov <emadov01@gmail.com>
2019-11-19 23:40:01 +0200

    Updates

---
+++ precache-manifest.8eb9cdb6fdc86045a6947e5b2b35f9c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "479afb4d03ca7b49b57dc36fbaa74178",
+    "url": "/Emad-eldin-Teama/mytodolist.git/index.html"
+  },
+  {
+    "revision": "13bfff1fac308cc5334c",
+    "url": "/Emad-eldin-Teama/mytodolist.git/static/css/main.a03d68c7.chunk.css"
+  },
+  {
+    "revision": "0aa5f1782e5860fda2db",
+    "url": "/Emad-eldin-Teama/mytodolist.git/static/js/2.8e3d1ec3.chunk.js"
+  },
+  {
+    "revision": "13bfff1fac308cc5334c",
+    "url": "/Emad-eldin-Teama/mytodolist.git/static/js/main.9b7edbae.chunk.js"
+  },
+  {
+    "revision": "f015884f6c91e69ddc3f",
+    "url": "/Emad-eldin-Teama/mytodolist.git/static/js/runtime-main.534c0737.js"
+  }
+]);
